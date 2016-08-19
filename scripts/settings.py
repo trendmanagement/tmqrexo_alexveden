d25e145a920c7658587d3be0cef37bdfb8e39d01
AlexVeden <i@alexveden.com>
2016-08-19 13:01:27 +0300

    - Mongo connstr fix

--- scripts/settings.py
+++ scripts/settings.py
@@ -26,7 +26,7 @@ EXO_LIST = [
 
 # MongoDB credentials
 #
-MONGO_CONNSTR = 'mongodb://localhost:27017/'
+MONGO_CONNSTR = 'mongodb://exowriter:qmWSy4K3@10.0.1.2/tmldb?authMechanism=SCRAM-SHA-1'
 MONGO_EXO_DB = 'tmldb'
 
 #
