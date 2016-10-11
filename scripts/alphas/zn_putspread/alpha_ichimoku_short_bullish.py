480678d5ccb1d03a5619d9512ec6c6a1fc80d821
Alex Veden <i@alexveden.com>
2016-10-11 17:36:03 +0300

    - Ichi rules index fix

--- scripts/alphas/zn_putspread/alpha_ichimoku_short_bullish.py
+++ scripts/alphas/zn_putspread/alpha_ichimoku_short_bullish.py
@@ -19,7 +19,7 @@ STRATEGY_CONTEXT = {
                         OptParam('base_line_period', 26, 20, 26, 1),
                         OptParam('leading_spans_lookahead_period', 26, 13, 13, 1),
                         OptParam('leading_span_b_period', 52, 52, 52, 10),
-                        OptParamArray('RulesIndex', np.arange(17)),
+                        OptParamArray('RulesIndex', np.arange(14)),
                         OptParam('MedianPeriod', 5,50, 50, 10)
             ],
     },
