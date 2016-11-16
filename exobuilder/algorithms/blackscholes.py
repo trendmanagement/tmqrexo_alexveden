- PayoffAnalyzer - payoff graphs calculation - PayoffAnalyzer - graphs plot and reports - BlackScholes CND fix - Position - underlying and underlying_price properties added

exobuilder/algorithms/blackscholes.py
@@ -11,7 +11,10 @@ def cnd(d):
     K = 1.0 / (1.0 + 0.2316419 * np.abs(d))
     ret_val = (RSQRT2PI * np.exp(-0.5 * d * d) *
                (K * (A1 + K * (A2 + K * (A3 + K * (A4 + K * A5))))))
-    return np.where(d > 0, 1.0 - ret_val, ret_val)
+    if d > 0:
+        return 1.0 - ret_val
+    else:
+        return ret_val
 
 def blackscholes(callputflag, ulprice, strike, toexpiry, riskfreerate, iv):
     try: