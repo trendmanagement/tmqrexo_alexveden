8961e9408c7c2448af1ed3cbb8b51e47ba8b8db0
spickering@tml1.com <NJoyce111174$>
2016-11-28 20:33:13 -0800

    added settings_connections.py

---
+++ scripts/settings_connections.py
@@ -0,0 +1,16 @@
+# MongoDB credentials
+#
+MONGO_CONNSTR = 'mongodb://exowriter:qmWSy4K3@10.0.1.2/tmldb?authMechanism=SCRAM-SHA-1'
+MONGO_EXO_DB = 'tmldb'
+
+#
+# SQL Server credentials
+SQL_HOST = 'h9ggwlagd1.database.windows.net'
+SQL_USER = 'modelread'
+SQL_PASS = '4fSHRXwd4u'
+
+#
+# RabbitMQ credentials
+RABBIT_HOST = 'localhost'
+RABBIT_USER = 'guest'
+RABBIT_PASSW = 'guest'
