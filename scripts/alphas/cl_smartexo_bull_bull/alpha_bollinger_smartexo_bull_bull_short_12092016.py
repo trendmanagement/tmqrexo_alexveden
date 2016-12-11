b56ef9c22d5221de380e4e1c32177cdd0ad9cfce
spickering@tml1.com <NJoyce111174$>
2016-12-10 22:01:22 -0800

    fixed settings.py

--- scripts/alphas/cl_smartexo_bull_bull/alpha_bollinger_smartexo_bull_bull_short_12092016.py
+++ scripts/alphas/cl_smartexo_bull_bull/alpha_bollinger_smartexo_bull_bull_short_12092016.py
@@ -12,7 +12,7 @@ STRATEGY_CONTEXT = {
     'strategy': {
         'class': StrategyBollingerBands,
         'exo_name': 'CL_SmartEXO_Ichi_DeltaTargeting_Dec3_Bull_Bull_Spread',
-    # <---- Select and paste EXO name from cell above 
+    # <---- Select and paste EXO name from cell above
         'opt_params': [
             # OptParam(name, default_value, min_value, max_value, step)
             OptParamArray('Direction', [-1]),
--- scripts/settings.py
+++ scripts/settings.py
@@ -4,6 +4,9 @@ from exobuilder.algorithms.exo_vertical_spread import EXOVerticalSpread
 from exobuilder.algorithms.exo_continous_fut import EXOContinuousFut
 from exobuilder.algorithms.smartexo_ichimoku_bear_straddle_150delta import SmartexoIchimokuBearStraddle150Delta
 from exobuilder.algorithms.smartexo_ichi_bullish_straddle_150delta_exphedged_nov22_2016 import SmartEXOIchiBullishStraddle150DeltaExpHedgedNov22_2016
+from exobuilder.algorithms.SmartEXO_Ichi_DeltaTargeting_Dec3_Bear_Bear_Spread import SmartEXO_Ichi_DeltaTargeting_Dec3_Bear_Bear_Spread
+from exobuilder.algorithms.SmartEXO_Ichi_DeltaTargeting_Dec3_Bi_Spread import SmartEXO_Ichi_DeltaTargeting_Dec3_Bi_Spread
+from exobuilder.algorithms.SmartEXO_Ichi_DeltaTargeting_Dec3_Bull_Bull_Spread import SmartEXO_Ichi_DeltaTargeting_Dec3_Bull_Bull_Spread
 
 #
 # Instruments list
@@ -37,6 +40,18 @@ EXO_LIST = [
         'name': 'SmartEXOIchiBullishStraddle150DeltaExpHedgedNov22_2016',
         'class': SmartEXOIchiBullishStraddle150DeltaExpHedgedNov22_2016,
     },
+    {
+        'name': 'SmartEXO_Ichi_DeltaTargeting_Dec3_Bear_Bear_Spread',
+        'class': SmartEXO_Ichi_DeltaTargeting_Dec3_Bear_Bear_Spread,
+    },
+    {
+        'name': 'SmartEXO_Ichi_DeltaTargeting_Dec3_Bi_Spread',
+        'class': SmartEXO_Ichi_DeltaTargeting_Dec3_Bi_Spread,
+    },
+    {
+        'name': 'SmartEXO_Ichi_DeltaTargeting_Dec3_Bull_Bull_Spread',
+        'class': SmartEXO_Ichi_DeltaTargeting_Dec3_Bull_Bull_Spread,
+    },
 
 ]
 
@@ -62,6 +77,9 @@ ALPHAS_CUSTOM = [
     'zw_putspread',
     'cl_smartexo_ichi_bearish_straddle_150delta',
     'cl_smartexo_ichi_bullish_straddle_150delta_exphedged_nov22_2016',
+    'cl_contfut',
+    'cl_smartexo_bi',
+    'cl_smartexo_bull_bull'
 ]
 
 # MongoDB credentials
