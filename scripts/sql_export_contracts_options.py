fdfde36c49da83b6a9f103f1cdf0131fb491b9ed
spickering@tml1.com <NJoyce111174$>
2017-03-10 19:03:00 -0800

    commit for data updating from mssql

---
+++ scripts/sql_export_contracts_options.py
@@ -0,0 +1,337 @@
+
+import sys, argparse, logging
+import pymssql
+import _mssql
+import datetime
+from decimal import Decimal
+import pymongo
+from pymongo import MongoClient
+from tqdm import tqdm, tnrange, tqdm_notebook
+
+from scripts.settings import *
+
+
+# SQL Server credentials
+SQL_HOST = 'h9ggwlagd1.database.windows.net'
+SQL_USER = 'steve'
+SQL_PASS = 'KYYAtv9P'
+
+#SQL_HOST = 'h9ggwlagd1.database.windows.net'
+#SQL_USER = 'modelread'
+#SQL_PASS = '4fSHRXwd4u'
+
+sql_conn = pymssql.connect(server=SQL_HOST, \
+                           user=SQL_USER + "@" + SQL_HOST, password=SQL_PASS, database='TMLDB')
+
+#sql_conn = pymssql.connect("h9ggwlagd1.database.windows.net", "modelread@h9ggwlagd1.database.windows.net", "4fSHRXwd4u", "TMLDB")
+
+def convert_dates(values):
+    k,v = values
+    if type(v) == datetime.date:
+        return k, datetime.datetime.combine(
+                v,
+                datetime.datetime.min.time())
+    if type(v) == Decimal:
+        return k, float(v)
+    if k == 'datetime' and type(v) == str:
+        return k, datetime.datetime.strptime(v, '%Y-%m-%d %H:%M:%S')
+    elif k == 'expirationdate' and type(v) == str:
+        return k, datetime.datetime.strptime(v, '%Y-%m-%d')
+    elif k == 'spanoptionstart' and type(v) == str:
+        return k, datetime.datetime.strptime(v, '%Y-%m-%d')
+    elif k == 'optionstart' and type(v) == str:
+        return k, datetime.datetime.strptime(v, '%Y-%m-%d')
+    elif k == 'datastart' and type(v) == str:
+        return k, datetime.datetime.strptime(v, '%Y-%m-%d')
+    elif k == 'customdayboundarytime' and type(v) == str:
+        return k, datetime.datetime.strptime(v, '%Y-%m-%d %H:%M:%S')
+    elif k == 'optioninputdatetime' and type(v) == str:
+        return k, datetime.datetime.strptime(v, '%Y-%m-%d %H:%M:%S')
+    if k == 'date' and type(v) == str:
+        return k, datetime.datetime.strptime(v, '%Y-%m-%d')
+    else:
+        return k, v
+
+#MONGO_CONNSTR = 'mongodb://exowriter:qmWSy4K3@10.0.1.2/tmldb?authMechanism=SCRAM-SHA-1'
+MONGO_CONNSTR = 'mongodb://tmqr:tmqr@10.0.1.2/tmldb_v2?authMechanism=SCRAM-SHA-1'
+MONGO_CONNSTR_LOCAL = 'mongodb://localhost:27017'
+MONGO_EXO_DB = 'tmldb_v2'
+MONGO_EXO_DB_LOCAL = 'tmldb'
+
+# Init mongo asset index
+client = MongoClient(MONGO_CONNSTR)
+mongo_db = client[MONGO_EXO_DB]
+
+#mongo_collection.create_index([('idbardata', pymongo.ASCENDING)], unique=True)
+#mongo_collection.create_index([('idcontract', pymongo.ASCENDING), ('datetime', pymongo.ASCENDING)], unique=True)
+
+print('contracts')
+############################################################################
+collection = mongo_db['contracts']
+
+qry = 'SELECT * FROM cqgdb.tblcontracts '
+logging.debug(qry)
+
+max_steps = 1
+pbar = tqdm(desc="Progress", total=max_steps)
+
+c2 = sql_conn.cursor(as_dict=True)
+c2.execute(qry)
+
+cnt = 0
+for row in c2:
+    try:
+        data = dict(map(convert_dates, row.items()))
+        collection.replace_one({'idcontract':data['idcontract']},data,upsert=True)
+        cnt += 1
+    except TypeError:
+        print('TypeError')
+        print(row)
+        break
+
+    pbar.update(1)
+
+print('Done {0} rows'.format(cnt))
+
+
+
+print('options')
+############################################################################
+collection = mongo_db['options']
+
+qry = 'SELECT * FROM cqgdb.tbloptions '
+logging.debug(qry)
+
+max_steps = 1
+pbar = tqdm(desc="Progress", total=max_steps)
+
+c2 = sql_conn.cursor(as_dict=True)
+c2.execute(qry)
+
+cnt = 0
+for row in c2:
+    try:
+        data = dict(map(convert_dates, row.items()))
+        collection.replace_one({'idoption':data['idoption']},data,upsert=True)
+        cnt += 1
+    except TypeError:
+        print('TypeError')
+        print(row)
+        break
+
+    pbar.update(1)
+
+print('Done {0} rows'.format(cnt))
+
+print('instruments')
+############################################################################
+collection = mongo_db['instruments']
+
+qry = 'SELECT * FROM cqgdb.tblinstruments '
+logging.debug(qry)
+
+max_steps = 1
+pbar = tqdm(desc="Progress", total=max_steps)
+
+c2 = sql_conn.cursor(as_dict=True)
+c2.execute(qry)
+
+cnt = 0
+for row in c2:
+    try:
+        data = dict(map(convert_dates, row.items()))
+        collection.replace_one({'idinstrument':data['idinstrument']},data,upsert=True)
+        cnt += 1
+    except TypeError:
+        print('TypeError')
+        print(row)
+        break
+
+    pbar.update(1)
+
+print('Done {0} rows'.format(cnt))
+
+
+print('option_input_data')
+############################################################################
+collection = mongo_db['option_input_data']
+
+qry = 'SELECT * FROM cqgdb.tbloptioninputdata '
+logging.debug(qry)
+
+max_steps = 1
+pbar = tqdm(desc="Progress", total=max_steps)
+
+c2 = sql_conn.cursor(as_dict=True)
+c2.execute(qry)
+
+cnt = 0
+for row in c2:
+    try:
+        data = dict(map(convert_dates, row.items()))
+        collection.replace_one({'idoptioninputsymbol':data['idoptioninputsymbol'],'optioninputdatetime':data['optioninputdatetime']},data,upsert=True)
+        cnt += 1
+    except TypeError:
+        print('TypeError')
+        print(row)
+        break
+
+    pbar.update(1)
+
+print('Done {0} rows'.format(cnt))
+
+
+
+print('futures_contract_settlements')
+############################################################################
+
+collection = mongo_db['futures_contract_settlements']
+
+qry = 'SELECT * FROM cqgdb.tbldailycontractsettlements '
+logging.debug(qry)
+
+max_steps = 1
+pbar = tqdm(desc="Progress", total=max_steps)
+
+c2 = sql_conn.cursor(as_dict=True)
+c2.execute(qry)
+
+contract_settlements = []
+
+cnt = 0
+for row in c2:
+    try:
+        future_settlements = dict(map(convert_dates, row.items()));
+
+        contract_settlements.append({'idcontract':future_settlements['idcontract'], \
+                                     'date':future_settlements['date'], \
+                                     'settlement':future_settlements['settlement'], \
+                                     'volume':future_settlements['volume'],
+                                     'openinterest':future_settlements['openinterest']})
+
+        cnt += 1
+    except TypeError:
+        print('TypeError')
+        print(row)
+        break
+
+    #if cnt > 3:
+    #    break
+
+    pbar.update(1)
+
+#print(contract_settlements)
+
+result  = collection.insert_many(contract_settlements)
+
+#print(result.inserted_ids)
+
+print('Done {0} rows'.format(cnt))
+
+
+print('options data')
+############################################################################
+collection = mongo_db['options_data']
+
+qry = 'SELECT * FROM cqgdb.tbloptiondata '
+logging.debug(qry)
+
+max_steps = 1
+pbar = tqdm(desc="Progress", total=max_steps)
+
+c2 = sql_conn.cursor(as_dict=True)
+c2.execute(qry)
+
+contract_settlements = []
+
+cnt = 0
+for row in c2:
+    try:
+        option_settlements = dict(map(convert_dates, row.items()));
+
+        contract_settlements.append({'idoption': option_settlements['idoption'], \
+                                     'datetime': option_settlements['datetime'], \
+                                     'price': option_settlements['price'], \
+                                     'impliedvol': option_settlements['impliedvol'], \
+                                     'timetoexpinyears': option_settlements['timetoexpinyears']})
+
+        if(cnt % 1000 == 0):
+            if(len(contract_settlements) > 0):
+                collection.insert_many(contract_settlements)
+
+            contract_settlements = []
+
+        cnt += 1
+    except TypeError:
+        print('TypeError')
+        print(row)
+        break
+
+    #if cnt > 3:
+    #    break
+
+    pbar.update(1)
+
+#print(contract_settlements)
+
+if(len(contract_settlements) > 0):
+    result  = collection.insert_many(contract_settlements)
+
+#print(result.inserted_ids)
+
+print('Done {0} rows'.format(cnt))
+
+
+print('Bar data')
+############################################################################
+collection = mongo_db['contracts_bars']
+
+qry = 'SELECT * FROM cqgdb.tblbardata '
+logging.debug(qry)
+
+max_steps = 1
+pbar = tqdm(desc="Progress", total=max_steps)
+
+c2 = sql_conn.cursor(as_dict=True)
+c2.execute(qry)
+
+contract_bars = []
+
+cnt = 0
+for row in c2:
+    try:
+        bar = dict(map(convert_dates, row.items()));
+
+        contract_bars.append({'datetime': bar['datetime'], \
+                                     'open': bar['open'], \
+                                     'high': bar['high'], \
+                                     'low': bar['low'], \
+                                     'close': bar['close'], \
+                              'idcontract': bar['idcontract'],
+                              })
+
+        if(cnt % 1000 == 0):
+            if(len(contract_bars) > 0):
+                collection.insert_many(contract_bars)
+
+            contract_bars = []
+
+        cnt += 1
+    except TypeError:
+        print('TypeError')
+        print(row)
+        break
+
+    #if cnt > 3:
+     #   break
+
+    pbar.update(1)
+
+#print(contract_settlements)
+
+if(len(contract_bars) > 0):
+    result  = collection.insert_many(contract_bars)
+
+#print(result.inserted_ids)
+
+print('Done {0} rows'.format(cnt))
