2dfd8c7179b880fe377111eb02201da6fbbf153c
spickering@tml1.com <NJoyce111174$>
2017-03-12 09:52:10 -0700

    commit for data updating from mssql option data and bar data only iteration chg

--- scripts/sql_export_contracts_options.py
+++ scripts/sql_export_contracts_options.py
@@ -227,7 +227,7 @@ result  = collection.insert_many(contract_settlements)
 #print(result.inserted_ids)
 
 print('Done {0} rows'.format(cnt))
-'''
+
 
 print('options data')
 ############################################################################
@@ -281,10 +281,14 @@ if(len(contract_settlements) > 0):
 
 print('Done {0} rows'.format(cnt))
 
+'''
 
 print('Bar data')
 ############################################################################
 collection = mongo_db['contracts_bars']
+
+collection.create_index([('idcontract',pymongo.ASCENDING),('datetime',pymongo.ASCENDING)])
+collection.create_index([('idcontract',pymongo.ASCENDING),('datetime',pymongo.DESCENDING)])
 
 qry = 'SELECT * FROM cqgdb.tblbardata '
 logging.debug(qry)
@@ -322,8 +326,8 @@ for row in c2:
         print(row)
         break
 
-    #if cnt > 3:
-     #   break
+    if cnt > 3:
+        break
 
     pbar.update(1)
 
