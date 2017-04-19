eafd15e724784e170a629b24a52a24f93c7cb3cf
AlexVeden <i@alexveden.com>
2017-04-19 22:28:35 +0400

    - Hybrid datasource client fix

--- exobuilder/data/datasource_hybrid.py
+++ exobuilder/data/datasource_hybrid.py
@@ -9,12 +9,12 @@ class DataSourceHybrid(DataSourceMongo):
     def __init__(self, mongo_connstr, mongo_db, assetindex, online_mongo_connstr, online_mongo_db, futures_limit, options_limit, exostorage=None):
         super().__init__(mongo_connstr, mongo_db, assetindex, futures_limit, options_limit, exostorage)
 
-        self.client = MongoClient(online_mongo_connstr)
-        self.db = self.client[online_mongo_db]
+        self.online_client = MongoClient(online_mongo_connstr)
+        self.online_db = self.online_client[online_mongo_db]
 
 
     def get_fut_data(self, dbid, date):
         try:
-            return self.db['futurebarcol'].find({'bartime': {'$lte': date}, 'idcontract': dbid, 'errorbar': False}).sort('bartime', pymongo.DESCENDING).next()
+            return self.online_db['futurebarcol'].find({'bartime': {'$lte': date}, 'idcontract': dbid, 'errorbar': False}).sort('bartime', pymongo.DESCENDING).next()
         except:
             raise QuoteNotFoundException('Futures data not found contract id: {0} date: {1}'.format(dbid, date))
