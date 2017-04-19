d1ebe7de14173eba75b05faf6585f67b132261b1
AlexVeden <i@alexveden.com>
2017-04-20 00:12:30 +0400

    - Reverted RFR stub

--- exobuilder/data/datasource_mongo.py
+++ exobuilder/data/datasource_mongo.py
@@ -49,17 +49,14 @@ class DataSourceMongo(DataSourceBase):
             #
             #  Getting risk-free-rate on previous day
             #
-            try:
-                rfr_result = self.db['option_input_data'].find({
-                    "idoptioninputsymbol": 15,
-                    "optioninputdatetime": {
-                        '$lt': self._shrink_datetime(date)}
-                }).sort([("optioninputdatetime", -1)]).limit(1).next()
+            rfr_result = self.db['option_input_data'].find({
+                "idoptioninputsymbol": 15,
+                "optioninputdatetime": {
+                    '$lt': self._shrink_datetime(date)}
+            }).sort([("optioninputdatetime", -1)]).limit(1).next()
 
-                rfr_dic[date] = rfr_result["optioninputclose"] / 100.0
-                return self.extra_data_cache[key][date]
-            except StopIteration:
-                return 1.27/100.0
+            rfr_dic[date] = rfr_result["optioninputclose"] / 100.0
+            return self.extra_data_cache[key][date]
         else:
             raise KeyError("Unknown key for extra_data, only 'riskfreerate' supported.")
 
