3582fd711100fd866246f368bca23f7bb6633e38
spickering@tml1.com <NJoyce111174$>
2017-04-24 13:01:41 -0700

    added CC and 6J

--- scripts/settings_exo.py
+++ scripts/settings_exo.py
@@ -22,7 +22,7 @@ from exobuilder.algorithms.SmartEXO_SWP_DeltaTargeting_1X1_Bi_2_3_2_2_neutralOnl
 #
 # Instruments list
 #
-INSTRUMENTS_LIST = ['ES', 'CL', 'NG', 'ZN', 'ZS', 'ZW', 'ZC', '6E', 'LBS', 'GC']
+INSTRUMENTS_LIST = ['ES', 'CL', 'NG', 'ZN', 'ZS', 'ZW', 'ZC', '6E', 'LBS', 'GC', 'CC', '6J']
 
 # Alphas list (generic)
 ALPHAS_GENERIC = ['alpha_exo']
