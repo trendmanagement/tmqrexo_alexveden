e0af2f3374fc5a57e18350d67301125d934108b8
spickering@outlook.com <NJoyce111174$>
2017-10-16 20:21:05 -0700

    removed XAF

--- scripts/settings_exo.py
+++ scripts/settings_exo.py
@@ -24,7 +24,7 @@ from exobuilder.algorithms.SmartEXO_SWP_DeltaTargeting_1X1_Bi_2_3_2_2_neutralOnl
 #
 INSTRUMENTS_LIST = ['ES','CL','NG','ZN','ZS','ZW','ZC','6E','LBS','GC','CC','6J',
                     'ZL','6B','LE','SB','6C','6A','RB','HO',
-                    'XAF','DC']
+                    'DC']
                     #'XAY', 'XAP', 'XAE', 'XAF', 'XAV', 'XAI', 'XAB', 'XAK', 'XAU']
 
 # Alphas list (generic)
