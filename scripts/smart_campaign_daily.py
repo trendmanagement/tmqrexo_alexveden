6ad6fc873df4696d3576545a77f62f6b34405972
spickering@outlook.com <NJoyce111174$>
2017-12-06 10:40:14 -0800

    smart_campaign_daily added the update of model data into the smart equity

--- scripts/smart_campaign_daily.py
+++ scripts/smart_campaign_daily.py
@@ -8,6 +8,9 @@ from scripts.settings import *
 from smartcampaign import SmartCampaignBase
 import traceback
 
+from backtester.reports.campaign_real_compare_report import CampaignRealCompare
+from datetime import datetime, date, timedelta, time as dttime
+
 from tradingcore.messages import *
 from tradingcore.signalapp import SignalApp, APPCLASS_UTILS
 
@@ -28,6 +31,72 @@ accounts_equity_collection = db['accounts_equity']
 # Update accounts linked with SmartCampaigns
 for acct_dict in accounts_collection.find({'mmclass_name': 'smart'}):
     signalapp.send(MsgStatus('RUN', 'Processing: {0}'.format(acct_dict), notify=False))
+
+    # *******************************************************************
+    '''
+    This is just for the test accounts with 'client_name': 'TEST'
+    Below takes the latest account equity from accounts_equity and pushes up a new value adjust with the model change  
+    '''
+    try:
+        num_of_days_back_master = 1
+
+        client = MongoClient(MONGO_CONNSTR)
+        db = client[MONGO_EXO_DB]
+        accounts_collection = db['accounts']
+        accounts_equity_collection = db['accounts_equity']
+
+        crc = CampaignRealCompare()
+
+        for acct_dict in accounts_collection.find({'mmclass_name': 'smart', 'client_name': 'TEST'}):
+
+            archive_based_pnl_dict = crc.get_account_positions_archive_pnl_multiproduct(
+                # costs_per_contract=3.0 # Default
+                # costs_per_option=3.0 # Default                                                          .
+                num_days_back=num_of_days_back_master,
+                fcm_office=acct_dict['FCM_OFFICE'], fcm_acct=acct_dict['FCM_ACCT'],
+                return_transactions=True,
+            )
+
+            total_pl_change = 0
+            for instrument, pnl_values in archive_based_pnl_dict.items():
+                total_pl_change += pnl_values['pnls']['SettleChange'][-1]
+
+            account_equity = list(
+                accounts_equity_collection.find({'name': acct_dict['name']}, {'equity_list': {'$slice': -2}}))
+            if account_equity:
+
+                equity_list = account_equity[0]['equity_list']
+
+                last_account_equity = account_equity[0]['equity_list'][-1]['equity']
+
+                if account_equity[0]['equity_list'][-1]['date'].date() != datetime.now().date():
+                    last_account_equity += total_pl_change
+                elif len(equity_list) > 1:
+                    last_account_equity = account_equity[0]['equity_list'][-2]['equity'] + total_pl_change
+
+                dt = datetime.combine(datetime.now().date(), dttime(0, 0, 0))
+
+                updateResult = accounts_equity_collection.update_one({'name': acct_dict['name']},
+                                                                     {
+                                                                         '$pull': {
+                                                                             'equity_list': {'date': dt},
+                                                                         }
+                                                                     }, upsert=True)
+
+                updateResult = accounts_equity_collection.update_one({'name': acct_dict['name']},
+                                                                     {
+                                                                         '$addToSet': {
+                                                                             'equity_list': {'date': dt,
+                                                                                             'equity': last_account_equity},
+                                                                         }
+                                                                     }, upsert=True)
+
+                # print('Updating accounts_equity collection account name {0} {1} smart equity {2}'
+                #       .format(acct_dict['name'], dt, last_account_equity))
+
+    except Exception as exc:
+        signalapp.send(MsgStatus('ERR', "Exception: \n\n" + traceback.format_exc(), notify=True))
+    # *******************************************************************
 
     account_equity_val = 100000
 
@@ -54,6 +123,11 @@ for acct_dict in accounts_collection.find({'mmclass_name': 'smart'}):
 
     except Exception as exc:
         signalapp.send(MsgStatus('ERR', "Exception: \n\n" + traceback.format_exc(), notify=True))
+
+
+
+
+
 
 
 signalapp.send(MsgStatus('RUN', 'Smart campaign updates finished', notify=True))
