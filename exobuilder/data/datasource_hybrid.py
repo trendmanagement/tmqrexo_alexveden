6819ed48e6d72ac98b4ba90c1dc781c429fdcc98
AlexVeden <i@alexveden.com>
2017-12-28 12:55:55 +0400

    - Eliminated 'futurebarcol' from the V1 - DataSourceHybrid() - will rise an error if initiated by code

--- exobuilder/data/datasource_hybrid.py
+++ exobuilder/data/datasource_hybrid.py
@@ -7,14 +7,4 @@ from .exceptions import QuoteNotFoundException
 
 class DataSourceHybrid(DataSourceMongo):
     def __init__(self, mongo_connstr, mongo_db, assetindex, online_mongo_connstr, online_mongo_db, futures_limit, options_limit, exostorage=None):
-        super().__init__(mongo_connstr, mongo_db, assetindex, futures_limit, options_limit, exostorage)
-
-        self.online_client = MongoClient(online_mongo_connstr)
-        self.online_db = self.online_client[online_mongo_db]
-
-
-    def get_fut_data(self, dbid, date):
-        try:
-            return self.online_db['futurebarcol'].find({'bartime': {'$lte': date}, 'idcontract': dbid, 'errorbar': False}).sort('bartime', pymongo.DESCENDING).next()
-        except:
-            raise QuoteNotFoundException('Futures data not found contract id: {0} date: {1}'.format(dbid, date))
+        raise Exception("This datasource type is obsolete please switch to DataSourceMongo")
