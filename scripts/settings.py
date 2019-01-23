3a21a017b99293ba7e63915ddb363228095aa84f
Alex Veden <i@alexveden.com>
2019-01-23 12:18:57 +0400

    - switched to localhost

--- scripts/settings.py
+++ scripts/settings.py
@@ -10,14 +10,8 @@
 
 # MongoDB credentials
 #
-MONGO_CONNSTR = 'mongodb://tmqr:tmqr@10.0.1.2/tmldb_v2?authMechanism=SCRAM-SHA-1'
+MONGO_CONNSTR = 'mongodb://localhost'
 MONGO_EXO_DB = 'tmldb_v2'
-
-#
-# SQL Server credentials
-SQL_HOST = 'h9ggwlagd1.database.windows.net'
-SQL_USER = 'modelread'
-SQL_PASS = '4fSHRXwd4u'
 
 #
 # RabbitMQ credentials
